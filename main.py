0ce6321fdbc2ee4bbf67861deb1952b38d5c6f92
juanico637 <jmshay11@gmail.com>
2023-05-21 10:29:55 +0800

    firstcommit

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ def find_sum(num1, num2):
     return sum_of_numbers
 
 class SumTestCase(unittest.TestCase):
+
     def test_find_sum(self):
+        #First Test
         result = find_sum(5, 7)
         self.assertEqual(result, 12)
 
+        #Second Test
         result = find_sum(-3, 10)
         self.assertEqual(result, 7)
 
